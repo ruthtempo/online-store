c221dc0e3f7ac63c3dbb10ce803f047a4cfa1c10
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-11 17:56:53 +0100

    Create TheHeader component (with dummy data)

---
+++ components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+        <h2>Logo</h2>
+        <nav>
+            <NuxtLink 
+                v-for="(number, index) in test" 
+                :key="index" 
+                :to="`/${number}`">
+                    test {{number}}
+            </NuxtLink>
+        </nav>
+        <div>
+            <div>UserStatus</div>
+            <div>Favorite</div>
+            <div>Cart</div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            test: [1,2,3,4] // (-> store.state.sections)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
