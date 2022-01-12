31929804d01fc0b7f926d9951a2219c7bb362050
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-12 10:20:54 +0100

    Create ProductCard component, rendering received object

---
+++ components/ProductCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+      {{ product }}
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        product: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
