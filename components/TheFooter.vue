c682da79c9760f0619b5072f3ce4eff9d502e40a
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-12 10:23:25 +0100

    Create TheFooter component

---
+++ components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+      Footer
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container {
+    background-color: #99990055;
+}
+</style>
