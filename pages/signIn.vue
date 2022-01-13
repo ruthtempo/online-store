e7778a96babfd03687f0d10ac21a0db3b97ebbdd
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-13 20:51:04 +0100

    Style (add margins)

--- pages/signIn.vue
+++ pages/signIn.vue
@@ -2,11 +2,11 @@
       <div class="form-container">
         <h2>Register</h2>
         <form class="register-form" @submit.prevent="handleSubmit">
-            <InputField type="text" label="First Name" v-model="user.firstName" />
-            <InputField type="text" label="Last Name" v-model="user.lastName" />
-            <InputField type="text" label="UserName" v-model="user.userName" />
-            <InputField type="email" label="Email" v-model="user.email" />
-            <InputField type="password" label="Password" v-model="user.password" />
+            <InputField class="input-field" type="text" label="First Name" v-model="user.firstName" />
+            <InputField class="input-field" type="text" label="Last Name" v-model="user.lastName" />
+            <InputField class="input-field" type="text" label="User Name" v-model="user.userName" />
+            <InputField class="input-field" type="email" label="Email" v-model="user.email" />
+            <InputField class="input-field" type="password" label="Password" v-model="user.password" />
             <div class="form-buttons">
                 <button>Register</button>
                 <NuxtLink to="/">Cancel</NuxtLink>
@@ -41,7 +41,9 @@ methods: {
     width: 80vw;
     margin: 0 auto 3rem;;
 }
-
+.input-field {
+    margin-top: 1rem;
+}
 .form-buttons {
     margin-top: 2rem;
 }
