5013a76e01c4e9b825bb5f82d1b80110f04a68ad
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-13 18:51:54 +0100

    Create InputField custom input component

---
+++ components/InputField.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="field">
+      <label :for="inputName">{{ label }}</label>
+      <input :type="type" :value="value" @input="$emit('input', $event.target.value)">
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        inputName() {
+            return this.label.toLowerCase().replace(/\s/g, '')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
