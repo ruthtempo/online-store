b9917610454b47bdf3279881625de1f8c699446f
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-13 18:53:17 +0100

    Create Sign In form

---
+++ pages/signIn.vue
@@ -0,0 +1,41 @@
+<template>
+      <div>
+        <h2>Register</h2>
+        <form @submit.prevent="handleSubmit">
+            <InputField type="text" label="First Name" v-model="user.firstName" />
+            <InputField type="text" label="Last Name" v-model="user.lastName" />
+            <InputField type="text" label="UserName" v-model="user.userName" />
+            <InputField type="email" label="Email" v-model="user.email" />
+            <InputField type="password" label="Password" v-model="user.password" />
+            <div class="form-group">
+                <button>Register</button>
+                <NuxtLink to="/">Cancel</NuxtLink>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        user: {
+            firstName: '',
+            lastName: '',
+            userName: '',
+            email: '',
+            password: ''
+        }
+    }
+},
+methods: {
+    handleSubmit(e) {
+        return
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
