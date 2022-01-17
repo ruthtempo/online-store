e4bee641f262676d7d8badc04a1b1d49d3b93c3b
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-17 18:45:08 +0100

    Create LogIn component

---
+++ components/LogIn.vue
@@ -0,0 +1,61 @@
+<template>
+    <ValidationObserver 
+        tag="form"
+        v-slot="{ invalid }"
+        class="register-form" 
+        @submit.prevent="handleLogIn">
+        <InputField
+            class="input-field"
+            type="email"
+            label="Email"
+            autocomplete="on"
+            veeValidateRules="required|email"
+            v-model="email" />
+        <InputField
+            class="input-field"
+            type="password"
+            label="Password"
+            autocomplete="off"
+            veeValidateRules="required|alpha_dash|min:8"
+            v-model="password" />
+        <button :disabled="invalid">Log In</button>
+    </ValidationObserver>
+</template>
+
+<script>
+import { ValidationObserver } from 'vee-validate';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+export default {
+    components: {
+        ValidationObserver
+    },
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        handleLogIn() {
+            const auth = getAuth();
+            console.log('Logging in: ' + this.email, this.password);
+            signInWithEmailAndPassword(auth, this.email, this.password)
+                .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                // ...
+                })
+                .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage)
+                });
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
