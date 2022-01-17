1385ba002a2a484fb551c3a09caa195444dd0bf0
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-17 12:48:14 +0100

    Install firebase and nuxtjs/firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,23 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyB60pSAqXYIhU_x767IdIJlvTmvJDJBtX0",
+          authDomain: "online-botiga.firebaseapp.com",
+          projectId: "online-botiga",
+          storageBucket: "online-botiga.appspot.com",
+          messagingSenderId: "884404975508",
+          appId: "1:884404975508:web:fd81ca6e38a0139b3b0181"
+        },
+        services: {
+          auth: true
+        },
+        terminateDatabasesAfterGenerate: true
+      }
+    ]
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
