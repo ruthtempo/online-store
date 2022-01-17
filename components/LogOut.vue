de28ddfa15eeff1724198b8bb15a0c86df409509
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-17 18:45:22 +0100

    Create LogOut component

---
+++ components/LogOut.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="handleLogOut">Log Out</button>
+</template>
+
+<script>
+import { getAuth, signOut } from "firebase/auth";
+export default {
+    methods: {
+        handleLogOut() {
+            const auth = getAuth();
+            console.log('Logging out');
+            signOut(auth).then(() => {
+            // Sign-out successful.
+            }).catch((error) => {
+            // An error happened.
+                console.log(error);
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
