444eaddc73684dfad65065e55c92b33f8cf49953
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-18 17:30:18 +0100

    Style and add fake content

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="container">
-      Footer
+  <div class="footer-container">
+      <ul class="list">
+        <li>Lorem</li>
+        <li>Ipsum</li>
+        <li>Lorem</li>
+        <li>Ipsum</li>
+      </ul>
   </div>
 </template>
 
@@ -11,8 +16,17 @@ export default {
 </script>
 
 <style scoped>
-.container {
+.footer-container {
     height:20%;
-    background-color: #99990055;
+    font-family: 'Source Code Pro', monospace;
+    background: rgb(0,111,126);
+    background: linear-gradient(354deg, rgb(44, 48, 53) 0%, rgba(7,7,98,1) 100%);
+    color: #ffc04a;
+}
+
+.list {
+  list-style: none;
+  width: 50vw;
+  margin: 2rem auto;
 }
 </style>
