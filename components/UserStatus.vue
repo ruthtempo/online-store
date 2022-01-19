02d704b895c372639506a952721a853392a256c0
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-19 16:45:04 +0100

    Create UserStatus component

---
+++ components/UserStatus.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="user-status-wrapper">
+        <div class="us-popover-title">
+            <nuxt-link to="/checkout"> 
+            <svg version="1.1" 
+                id="Layer_1" 
+                xmlns="http://www.w3.org/2000/svg" 
+                xmlns:xlink="http://www.w3.org/1999/xlink" 
+                x="0px" y="0px"
+	            width="30px" height="30px" 
+                viewBox="0 0 92 92" enable-background="new 0 0 92 92" xml:space="preserve">
+                <path id="XMLID_1253_" d="M46,57.1c14.8,0,26.9-12.1,26.9-27C72.9,15.1,60.8,3,46,3S19.1,15.1,19.1,30C19.1,45,31.2,57.1,46,57.1z
+	                M46,11c10.4,0,18.9,8.5,18.9,19c0,10.5-8.5,19-18.9,19s-18.9-8.5-18.9-19C27.1,19.5,35.6,11,46,11z M58.5,59.7
+	                c-1.3-0.4-2.8-0.1-3.8,0.8L46,67.9l-8.7-7.4c-1.1-0.9-2.5-1.2-3.8-0.8C27.9,61.5,0,71.1,0,85c0,2.2,1.8,4,4,4h84c2.2,0,4-1.8,4-4
+	                C92,71.1,64.1,61.5,58.5,59.7z M10.1,81c4.4-4.7,15-9.9,23.8-12.9l9.5,8.1c1.5,1.3,3.7,1.3,5.2,0l9.5-8.1
+	                c8.8,3.1,19.4,8.3,23.8,12.9H10.1z"/>
+            </svg>
+            </nuxt-link>
+        </div>
+        <div class="us-popover-content">
+            {{user ? `You're logged in (${user.email})` : "You're not logged in" }}
+            <div v-if="!user" class="user-not-logged-in">
+                <LogIn />
+                <p>Don't have an account? <NuxtLink to="/sign-in">Sign In</NuxtLink></p>
+            </div>
+            <LogOut v-if="user"/>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"UserStatus",
+    computed: {
+        user() {
+        return this.$store.state.user
+        }
+    }
+}
+</script>
+
+<style scoped>
+.user-status-wrapper {
+  background-color: transparent;
+  max-width:300px;
+  margin: 0 auto
+}
+.user-status-wrapper:hover .us-popover-content {
+  z-index: 10;
+  opacity: 1;
+  visibility: visible;
+  transform: translate(0, -20px);
+  transition: all 0.5s cubic-bezier(0.75, -0.02, 0.2, 0.97);
+}
+.us-popover-content {
+    opacity: 0;
+    visibility: hidden;
+    position: absolute;
+    background: rgb(0,111,126);
+    background: linear-gradient(354deg, rgb(44, 48, 53) 0%, rgba(7,7,98,1) 100%);
+    border: 2px dashed #ffc04a;
+    right:100px;
+    padding: 1.5rem;
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
+    width: 35ch;
+}
+path {
+    fill: #ffc04a;
+}
+a {
+    text-decoration: none;
+    color:white;
+}
+h3, h4 {
+  font-size: 15px;
+}
+button {
+  background-color: transparent;
+}
+</style>
