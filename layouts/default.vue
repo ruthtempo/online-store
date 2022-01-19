9741a57f101e93cfdbae5d7a14c573091dcd75d7
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-19 18:56:32 +0100

    Add TheSideNav and logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="page">
       <TheHeader />
+      <TheSideNav />
       <div class="main-content">
         <Nuxt />
       </div>
       <TheFooter />
   </div>
 </template>
+
+<script>
+import TheHeader from '~/components/TheHeader'
+import TheFooter from '~/components/TheFooter'
+import TheSideNav from '~/components/TheSideNav'
+export default {
+    components: {
+        TheHeader,
+        TheSideNav,
+        TheFooter
+    },
+    computed: {
+        sidenav() {
+            return this.$store.getters.getSideNavStatus
+        }
+    },
+    watch: {
+        '$route': function() {
+            if (process.client && this.sidenav && window.innerWidth < 1420) {
+                this.$store.dispatch('toggleSideNav')
+            }
+        }
+    }
+}
+</script>
+
 <style>
 html, body {
   height: 100vh;
-  width: 100vw;
   margin: 0;
   padding: 0;
   font-family: 'Montserrat', sans-serif;
