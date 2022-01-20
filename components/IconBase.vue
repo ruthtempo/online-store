446eb7380ca7ff058f076c9a1d9ca31064e63770
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-20 18:52:23 +0100

    Create IconBase component

---
+++ components/IconBase.vue
@@ -0,0 +1,36 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg"
+        :width="width" 
+        :height="height"
+        viewBox="0 0 92 92"
+        :aria-labelledby="iconName"
+        role="presentation"
+    >
+        <g :fill="strokeColor">
+            <slot></slot>
+        </g>
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        iconName: {
+            type: String,
+            default: 'icon'
+        },
+        width: {
+            type: [Number, String],
+            default: 30
+        },
+        height: {
+            type: [Number, String],
+            default: 30
+        },
+        strokeColor: {
+            type: String,
+            default: 'currentColor'
+        }
+    }
+}
+</script>
