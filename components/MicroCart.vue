736e4af39d0bde3771fdebc63253e0b3837d3000
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-20 19:19:20 +0100

    Import IconCart component

--- components/MicroCart.vue
+++ components/MicroCart.vue
@@ -2,20 +2,7 @@
   <div class="microcart-wrapper">
     <div class="cart-popover-title">
       <nuxt-link to="/checkout"> 
-        <svg version="1.1" 
-          id="Layer_1" 
-          xmlns="http://www.w3.org/2000/svg" 
-          xmlns:xlink="http://www.w3.org/1999/xlink" 
-          x="0px" y="0px"
-	        width="30px" height="30px" 
-          viewBox="0 0 92 92" enable-background="new 0 0 92 92" xml:space="preserve">
-          <path id="XMLID_1732_" d="M91.8,27.3L81.1,61c-0.8,2.4-2.9,4-5.4,4H34.4c-2.4,0-4.7-1.5-5.5-3.7L13.1,19H4c-2.2,0-4-1.8-4-4
-	        s1.8-4,4-4h11.9c1.7,0,3.2,1.1,3.8,2.7L36,57h38l8.5-27H35.4c-2.2,0-4-1.8-4-4s1.8-4,4-4H88c1.3,0,2.5,0.7,3.2,1.7
-	        C92,24.7,92.2,26.1,91.8,27.3z M36.4,70.3c-1.7,0-3.4,0.7-4.6,1.9c-1.2,1.2-1.9,2.9-1.9,4.6c0,1.7,0.7,3.4,1.9,4.6
-	        c1.2,1.2,2.9,1.9,4.6,1.9s3.4-0.7,4.6-1.9c1.2-1.2,1.9-2.9,1.9-4.6c0-1.7-0.7-3.4-1.9-4.6C39.8,71,38.1,70.3,36.4,70.3z M72.3,70.3
-	        c-1.7,0-3.4,0.7-4.6,1.9s-1.9,2.9-1.9,4.6c0,1.7,0.7,3.4,1.9,4.6c1.2,1.2,2.9,1.9,4.6,1.9c1.7,0,3.4-0.7,4.6-1.9
-	        c1.2-1.2,1.9-2.9,1.9-4.6c0-1.7-0.7-3.4-1.9-4.6S74,70.3,72.3,70.3z"/>
-        </svg>
+        <IconBase icon-name="cart" strokeColor="#ffc04a"><IconCart/></IconBase>
         ({{$store.state.cart.length}})
       </nuxt-link>
     </div>
@@ -42,17 +29,25 @@
             </button>
         </div>
         <div v-else>
-            <img src="../assets/cart.svg" alt="">
+            <IconBase icon-name="cart" height="90" width="90" strokeColor="#ffc04a"><IconCart/></IconBase>
             <h4>Your Cart is Empty</h4>
         </div>
     </div>
   </div>
 </template>
+
 <script>
+import IconBase from '~/components/IconBase'
+import IconCart from '~/components/icons/IconCart'
 export default {
-  name:"MicroCart"
+  name:"MicroCart",
+  components: {
+    IconBase,
+    IconCart
+  }
 }
 </script>
+
 <style scoped>
 
 div{
