419ff6555ea7b5f8a9af826135ec9b8ef048a9e1
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-27 16:00:41 +0100

    Change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'online-botiga',
+    title: 'PixelFashion',
     htmlAttrs: {
       lang: 'en'
     },
