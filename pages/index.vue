46cf2ccebcbb2158d5c4b0d7d8a43e12fe6688f8
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-01-28 19:34:12 +0100

    Add content

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,71 @@
 	TEMPLATE
 	************************************************************************* -->
 <template>
-  <div>Index</div>
+  <div class="index-container">
+	<div class="banner-div">
+		<NuxtLink to="/women's%20clothing">
+			<img src="~/assets/w-clothing-banner.jpg" alt="Women's clothing" class="banner-img">
+		</NuxtLink>
+	</div>
+	<div class="banner-div">
+		<NuxtLink to="/electronics">
+			<img src="~/assets/electronics-banner.jpg" alt="Electronics" class="banner-img">
+		</NuxtLink>
+	</div>
+	<div class="banner-div">
+		<NuxtLink to="/men's%20clothing">
+			<img src="~/assets/m-clothing-banner.jpg" alt="Men's clothing" class="banner-img">
+		</NuxtLink>
+	</div>
+	<div class="banner-div">
+		<NuxtLink to="/jewelery">
+			<img src="~/assets/jewelery-banner.jpg" alt="Jewelery" class="banner-img">
+		</NuxtLink>
+	</div>
+  </div>
 </template>
-
-<!-- *************************************************************************
-	SCRIPT
-	************************************************************************* -->
-
-<script>
-export default {};
-</script>
 
 <!-- *************************************************************************
 	STYLE
 	************************************************************************* -->
 
 <style scoped>
+.index-container {
+	display: flex;
+	flex-wrap: wrap;
+	width: max(364px, 90vw);
+	padding-top: 0;
+	margin-bottom: 2rem;
+}
+.banner-img {
+	height: auto;
+	max-height: 450px;
+	width: 100%;
+	max-width: 650px;
+	border: 2px solid #070762;
+	box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.26);
+}
+.banner-div {
+	margin: auto;
+}
+@media (min-width: 390px) {
+	.index-container {
+		margin: auto;
+	}
+@media (min-width: 435px) {
+	.index-container{
+	margin: 2vw auto;
+	}
+	.banner-div {
+		margin-bottom: 1rem;
+	}
+}
+}
+
+@media (min-width: 1700px) {
+	.banner-img {
+		max-width: 750px;
+	}
+}
 </style>
   
